814b1a01c0353a6a1e1031aef434b739ebf70128
5656baba <syounenjampsan@gmail.com>
2022-06-18 09:11:46 +0000

    maked to do list

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,16 @@
+class GenresController < ApplicationController
+  def index
+    @genres = Genre.all
+    @genre = Genre.new
+  end
+
+  def create
+    Genre.create(genre_params)
+    @genres = Genre.all
+  end
+
+  private
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+end
