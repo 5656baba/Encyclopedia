244a19a23c5d8a5f6bc3fe2426031435d4d9b2b3
5656baba <syounenjampsan@gmail.com>
2022-06-22 15:00:27 +0000

    Addition of validation and deletion functions

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,5 @@
 class Genre < ApplicationRecord
   has_many :summaries, dependent: :destroy
+
+  validates :name, presence: true
 end
--- app/models/summary.rb
+++ app/models/summary.rb
@@ -1,5 +1,8 @@
 class Summary < ApplicationRecord
   belongs_to :genre
+  validates :title, presence: true
+  validates :sentence, presence: true
+  validates :genre_id, presence: true
 
   def self.search(search)
     Summary.where(["title like? OR sentence like?", "%#{search}%", "%#{search}%"])
